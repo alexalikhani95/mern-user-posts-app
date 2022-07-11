06ae0234187b827b71f11afb1cb4c8dce9887c66
Alex Alikhani <alikhanialex@gmail.com>
2022-07-11 19:39:49 +0100

    Connecting controller functions that use the same route

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -2,12 +2,7 @@ const express = require("express");
 const router = express.Router(); // Router instance - complete middleware and routing system
 const { getPosts, createPost, editPost, deletePost } = require("../controllers/postController");
 
-router.get("/", getPosts);
-
-router.post("/", createPost);
-
-router.put("/:id", editPost);
-
-router.delete("/:id", deletePost);
+router.route("/").get(getPosts).post(createPost);
+router.route("/:id").delete(deletePost).put(editPost);
 
 module.exports = router;
