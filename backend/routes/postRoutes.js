bce5baa834b3ca62b22c0402aef29a8596269307
Alex Alikhani <alikhanialex@gmail.com>
2022-07-11 19:13:39 +0100

    Creating post, put and delete routes for posts

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -5,4 +5,16 @@ router.get("/", (req, res) => {
   res.send({ message: "Get Posts!" });
 });
 
+router.post("/", (req, res) => {
+  res.send({ message: "Create posts" });
+});
+
+router.put("/:id", (req, res) => {
+  res.send({ message: `Update post ${req.params.id}` });
+});
+
+router.delete("/:id", (req, res) => {
+  res.send({ message: `Update post ${req.params.id}` });
+});
+
 module.exports = router;
