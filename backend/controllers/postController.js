32455be6cd971c8940cddb42261d8c66d4a05bca
Alex Alikhani <alikhanialex@gmail.com>
2022-07-14 18:56:15 +0100

    Error and exception handling

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,26 +1,30 @@
 // Get posts
 // Route - GET api/posts
 const getPosts = (req, res) => {
-  res.send({ message: "Get posts" });
+  res.status(200).json({ message: "Get posts" });
 };
 
 // Create post
 // route - POST /api/posts
 const createPost = (req, res) => {
-  console.log(req.body);
-  res.send({ message: "Create posts" });
+  // If no body text, create a bad request status
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+  res.status(200).json({ message: "Create Posts" });
 };
 
 // Edit post
 // Route - PUT /api/posts/:id
 const editPost = (req, res) => {
-  res.send({ message: `Edit post ${req.params.id}` });
+  res.status(200).json({ message: `Edit post ${req.params.id}` });
 };
 
 // delete post
 // route - DELETE /api/posts/:id
 const deletePost = (req, res) => {
-  res.send({ message: `Delete post ${req.params.id}` });
+  res.status(200).json({ message: `Delete post ${req.params.id}` });
 };
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // body parser for raw json
 app.use(express.urlencoded({ extended: false })); // built-in middleware function in Express. It parses incoming requests with urlencoded payloads and is based on body-parser.
 
 app.use("/api/posts", require("./routes/postRoutes"));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
