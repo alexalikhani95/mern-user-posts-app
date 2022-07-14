4c3a9adb06fe39d902fbe21a41d07552e5bb3a33
Alex Alikhani <alikhanialex@gmail.com>
2022-07-14 19:07:42 +0100

    Making goalController functions asynchronous

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,31 +1,35 @@
+//Using async handler to avoid using try/catches and just use the err handlers
+const asyncHandler = require("express-async-handler");
+
 // Get posts
 // Route - GET api/posts
-const getPosts = (req, res) => {
+const getPosts = asyncHandler(async (req, res) => {
+  //Using async with the postcontroller functions as a promise will be sent when mongoose is used with the goalcontroller functions.
   res.status(200).json({ message: "Get posts" });
-};
+});
 
 // Create post
 // route - POST /api/posts
-const createPost = (req, res) => {
+const createPost = asyncHandler(async (req, res) => {
   // If no body text, create a bad request status
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field");
   }
   res.status(200).json({ message: "Create Posts" });
-};
+});
 
 // Edit post
 // Route - PUT /api/posts/:id
-const editPost = (req, res) => {
+const editPost = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Edit post ${req.params.id}` });
-};
+});
 
 // delete post
 // route - DELETE /api/posts/:id
-const deletePost = (req, res) => {
+const deletePost = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete post ${req.params.id}` });
-};
+});
 
 module.exports = {
   getPosts,
