be7ba0e7f02a5da95bc049956cf5b0927ecd3559
Alex Alikhani <alikhanialex@gmail.com>
2022-07-15 20:41:55 +0100

    JWT is now generated for the user when they they register/login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -30,6 +30,7 @@ const registerUser = asyncHandler(async (req, res) => {
     name,
     email,
     password: hashedPassword,
+    token: generateToken(user._id),
   });
 
   if (user) {
@@ -60,6 +61,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -72,6 +74,14 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "User data display" });
 });
+
+// Generate JWT
+const generateToken = (id) => {
+  // assign a new token with the id thats passed in, using the secret and it will expire in 30 days
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = {
   registerUser,
