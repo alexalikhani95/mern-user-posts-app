ce4fcb44b4f6c1e6b6ead059e2864c4825fb529c
Alex Alikhani <alikhanialex@gmail.com>
2022-07-15 20:23:29 +0100

    Hash the users password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,7 +12,38 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400); // bad request if no name, email or password
     throw new Error("Please add all fields");
   }
-  res.json({ message: "Register User" });
+
+  const userExists = await User.findOne({ email }); // Find the user by the email passed in
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("USer already exists");
+  }
+
+  // Hash the password
+  const salt = await bcrypt.genSalt(10); // A salt needs to be generated to has password (bcrypt method). 10 here is the number of rounds
+
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    // If the user is now created
+    res.status(201).json({
+      // created status
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 //Authenticate a user
