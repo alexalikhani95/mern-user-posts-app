7c84ac962c205e9fb0f2552e2208013a7b386ee6
Alex Alikhani <alikhanialex@gmail.com>
2022-07-16 17:01:40 +0100

    Creating basic Register, login, dashboard pages and installing react router for rotuing of these pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./App.css";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Diary App</div>;
+  return (
+    <>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
