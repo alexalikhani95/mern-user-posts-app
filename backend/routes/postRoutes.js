75457d2d94e4c28226ad8350f4940d50a1a91ad3
Alex Alikhani <alikhanialex@gmail.com>
2022-07-16 15:54:18 +0100

    Protecting all the post routes

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router(); // Router instance - complete middleware and routing system
 const { getPosts, createPost, editPost, deletePost } = require("../controllers/postController");
+const { protect } = require("../middleware/authMiddleware");
 
-router.route("/").get(getPosts).post(createPost);
-router.route("/:id").delete(deletePost).put(editPost);
+router.route("/").get(protect, getPosts).post(protect, createPost);
+router.route("/:id").delete(protect, deletePost).put(protect, editPost);
 
 module.exports = router;
